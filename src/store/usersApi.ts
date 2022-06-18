1d66e6da2990adbcab796be99b4b8c98804f1637
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-18 21:10:29 +0300

    Add function for fetching users

---
+++ src/store/usersApi.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { Users } from "./currentUsersSlice";
+
+export const usersApi = createApi({
+	reducerPath: "usersApi",
+	baseQuery: fetchBaseQuery({baseUrl: "https://frontend-test-assignment-api.abz.agency/api/v1"}),
+	tagTypes: ["Users"],
+	endpoints: (builder) => ({
+		getUsers: builder.query<Users, Users>({
+			query: (users) => (`users?page=${users.page}&count=${users.count}`),
+			providesTags: [{type: "Users", id: "List"}],
+		})
+	}),
+});
+
+export const { useGetUsersQuery } = usersApi;
