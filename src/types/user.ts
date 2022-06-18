c75298bb93791fff6c7b0ceeb557df48fa983343
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-18 21:07:40 +0300

    Create user type

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+
+export type User = {
+	"id": string,
+	"name": string,
+	"email": string,
+	"phone": string,
+	"position": string,
+	"position_id": string,
+	"registration_timestamp": string,
+	"photo": string,
+};
