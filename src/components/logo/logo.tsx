3a19a40f78b222b9517c07cc066197a4a8bdecb1
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-19 00:01:59 +0300

    Add logo

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import "./logo.scss";
+import LogoImages from "../../images/logo.svg"
+
+export const Logo: FC = () => {
+	return (
+		<a className="logo" href="#home">
+			<img
+				className="logo__image"
+				src={LogoImages}
+				alt="TestTask logo"
+			/>
+		</a>
+	);
+};
