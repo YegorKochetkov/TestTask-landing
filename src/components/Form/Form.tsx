ef0b143883b1b9a08f1769870f825452e2ccd1da
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-23 16:22:29 +0300

    Add submitting functional at form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,21 @@
 import React, { FC, useMemo, useState } from "react";
 import "./Form.scss";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FormikState, FormikHelpers } from "formik";
 import { FormikField } from "../FormikField";
 import { FormikOption } from "../FormikOption/FormikOption";
 import { SignUpSchema } from "./SingUpSchema";
 import { CustomTooltip } from "../Form/CustomTooltip";
 import classNames from "classnames";
+import { UpdateUser, useAddUserMutation, useGetTokenQuery } from "../../store/usersApi";
+import { Registered } from "../Registered";
+import { Loader } from "../Loader";
+import { FormTextFields } from "./FormTextFields";
 
-interface FormValues {
+export interface FormValues {
 	name: string,
-	mail: string,
+	email : string,
 	phone: string,
-	position: string,
+	position_id: number,
   photo: string,
 }
 
@@ -21,132 +25,140 @@ type UploadEvent = {
 
 const initialValues: FormValues = {
 	name: "",
-	mail: "",
+	email : "",
 	phone: "",
-	position: "",
+	position_id: 0,
   photo: "",
 };
 
 export const AddNewUserForm: FC = () => {	
   const [uploadText, setUploadText] = useState("Upload your photo");
+  const { data } = useGetTokenQuery();
+  const [addUser, { isError, isLoading }] = useAddUserMutation();
+
+  const [isRegistered, setIsRegistered] = useState(false);
 
   const isTouchedUploadField = useMemo(() => {
     return uploadText !== "Upload your photo";
   }, [uploadText]);
 
-	const handleSubmit = (values: FormValues): void => {
-		console.log(JSON.stringify(values));
+	const handleSubmit = async (
+    values: FormValues,
+    actions: FormikHelpers<FormValues>
+  ) => {
+    const formFields = Object.entries(values);
+    const userForm = new FormData();
+
+    for (const field of formFields) {
+      userForm.append(field[0], field[1]);
+    }
+
+    if (data) {
+      const newUser: UpdateUser = {
+        newUser: userForm,
+        token: data.token,
+      };
+
+      await addUser(newUser).unwrap();
+      actions.resetForm();
+      setIsRegistered(true);
+    }
 	};
 
   const handleUpload = (event: UploadEvent): void => {
-    setUploadText(event.currentTarget.files[0].name);
+    setUploadText(event.currentTarget.files[0]?.name);
   };
 
 	return (
-		<section className="form">
-			<h2 className="form__title">
-				Working with POST request
-			</h2>
+    <>
+      <section className="form">
+        <h2 className="form__title">
+          Working with POST request
+        </h2>
 
-			<Formik
-				initialValues={initialValues}
-				onSubmit={handleSubmit}
-        validationSchema={SignUpSchema}
-			>
-				{({ dirty, isValid, errors, touched, setFieldValue }) => {
-					return (
-            <>
-              <Form className="form__field">
-                <FormikField
-                  label="Your name"
-                  name="name"
-                  errors={errors}
-                  touched={touched}
-                />
-              </Form>
+        <div className="form__loader">
+          {isLoading && <Loader />}
+        </div>
 
-              <Form className="form__field">
-                <FormikField
-                  label="Email"
-                  name="mail"
-                  errors={errors}
-                  touched={touched}
-                />
-              </Form>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values, actions) => handleSubmit(values, actions)}
+          validationSchema={SignUpSchema}
+        >
+          {({ dirty, isValid, errors, touched, setFieldValue }) => {
+            return (
+              <>
+                <FormTextFields errors={errors} touched={touched} />
 
-              <Form className="form__field">
-                <FormikField
-                  label="Phone"
-                  name="phone"
-                  errors={errors}
-                  touched={touched}
-                />
-              </Form>
-              
-              <Form className="form__field">
-                <label className="form__label">
-                  Select your position
-                </label>
-                <FormikOption
-                  name="position"
-                />
-                <ErrorMessage name="position" />
-              </Form>
-
-              <Form
-                className={classNames(
-                  "form__field form__field--upload",
-                  {"form__field--error":
-                    isTouchedUploadField && errors.photo},
-                )}
-              >
-                <label>
-                  <Field
-                    name="photo"
-                    type="file"
-                    value={undefined}
-                    onChange={(event: UploadEvent) => {
-                      setFieldValue("photo", event.currentTarget.files[0]);
-                      handleUpload(event);
-                    }}
-                    hidden
+                <Form className="form__field">
+                  <label className="form__label">
+                    Select your position
+                  </label>
+                  <FormikOption
+                    name="position_id"
                   />
+                  <ErrorMessage name="position_id" />
+                </Form>
 
-                  <CustomTooltip title={uploadText}>
-                    <div>
-                      <p
-                        className={classNames(
-                          "form__upload-button",
-                          {"form__upload-button--error":
-                            isTouchedUploadField && errors.photo},
-                        )}
-                      >
-                        Upload
-                      </p>
-                      <p className="form__upload-text">
-                        {uploadText}
-                      </p>
-                    </div>
-                  </CustomTooltip>
-                </label>
-                <p className="form__error-message">
-                  {isTouchedUploadField && errors.photo}
-                </p>
-              </Form>
+                <Form
+                  className={classNames(
+                    "form__field form__field--upload",
+                    {"form__field--error":
+                      isTouchedUploadField && errors.photo},
+                  )}
+                >
+                  <label>
+                    <Field
+                      name="photo"
+                      type="file"
+                      value={undefined}
+                      onChange={(event: UploadEvent) => {
+                        setFieldValue("photo", event.currentTarget.files[0]);
+                        handleUpload(event);
+                      }}
+                      hidden
+                    />
 
-              
-              <button
-                type="submit"
-                id="signUp"
-                disabled={!dirty || !isValid}
-                className="button"
-              >
-                Sign up
-              </button>
-            </>
-					);
-				}}
-			</Formik>
-		</section>
+                    <CustomTooltip title={uploadText}>
+                      <div>
+                        <p
+                          className={classNames(
+                            "form__upload-button",
+                            {"form__upload-button--error":
+                              isTouchedUploadField && errors.photo},
+                          )}
+                        >
+                          Upload
+                        </p>
+                        <p className="form__upload-text">
+                          {uploadText}
+                        </p>
+                      </div>
+                    </CustomTooltip>
+                  </label>
+                  
+                  <p className="form__error-message">
+                    {isTouchedUploadField && errors.photo}
+                  </p>
+                </Form>
+
+                <Form>
+                  <button
+                    type="submit"
+                    id="signUp"
+                    disabled={!dirty || !isValid || isLoading}
+                    className="button"
+                  >
+                    Sign up
+                  </button>
+                </Form>
+              </>
+            );
+          }}
+        </Formik>
+      </section>
+
+      {isRegistered && <Registered />}
+    </>
 	);
 };
