60d0ed54628409fbc3a4b6405883aa93afdcc23e
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-23 04:13:40 +0300

    Implement to form upload file field

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from "react";
+import React, { FC, useMemo, useState } from "react";
 import "./Form.scss";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 import { FormikField } from "../FormikField";
 import { FormikOption } from "../FormikOption/FormikOption";
+import { SignUpSchema } from "./SingUpSchema";
+import { CustomTooltip } from "../Form/CustomTooltip";
+import classNames from "classnames";
 
 interface FormValues {
 	name: string,
@@ -13,6 +15,10 @@ interface FormValues {
   photo: string,
 }
 
+type UploadEvent = {
+  currentTarget: { files: { name: React.SetStateAction<string>; }[]; };
+};
+
 const initialValues: FormValues = {
 	name: "",
 	mail: "",
@@ -21,48 +27,20 @@ const initialValues: FormValues = {
   photo: "",
 };
 
-const mailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-const phoneRegex = /^[\+]{0,1}380([0-9]{9})$/;
-const maxFileSize = 5000000;
-const supportedFiles = ["jpg", "jpeg"];
-
-const SignUpSchema = Yup.object().shape({
-  name: Yup
-    .string()
-    .min(2, "Too short!")
-    .max(60, "Too long!")
-    .required("Name is required"),
-  mail: Yup
-    .string()
-    .matches(mailRegex, "Invalid email")
-    .min(2, "Too short!")
-    .max(100, "Too long!")
-    .required("Mail is required"),
-  phone: Yup
-    .string()
-    .matches(phoneRegex, "Invalid phone")
-    .required("Phone is required"),
-  position: Yup
-    .string()
-    .required("Please, choose a position"),
-  photo: Yup
-    .string()
-    // .mixed()
-    // .test("fileSize", "File is too large", (files) => {
-    //   const size = files[0].size / 1024 / 1024;
-    //   console.log(size)
-    //   return size < 5;
-    // })
-    // .test("fileFormat", "We support only jpeg/jpg", (files) => {
-    //   return supportedFiles.includes(files[0].type);
-    // })
-    .required("Please, upload your photo"),
-});
-
 export const AddNewUserForm: FC = () => {	
+  const [uploadText, setUploadText] = useState("Upload your photo");
+
+  const isTouchedUploadField = useMemo(() => {
+    return uploadText !== "Upload your photo";
+  }, [uploadText]);
+
 	const handleSubmit = (values: FormValues): void => {
 		console.log(JSON.stringify(values));
-	}
+	};
+
+  const handleUpload = (event: UploadEvent): void => {
+    setUploadText(event.currentTarget.files[0].name);
+  };
 
 	return (
 		<section className="form">
@@ -75,7 +53,7 @@ export const AddNewUserForm: FC = () => {
 				onSubmit={handleSubmit}
         validationSchema={SignUpSchema}
 			>
-				{({ dirty, isValid, errors, touched }) => {
+				{({ dirty, isValid, errors, touched, setFieldValue }) => {
 					return (
             <>
               <Form className="form__field">
@@ -86,6 +64,7 @@ export const AddNewUserForm: FC = () => {
                   touched={touched}
                 />
               </Form>
+
               <Form className="form__field">
                 <FormikField
                   label="Email"
@@ -94,6 +73,7 @@ export const AddNewUserForm: FC = () => {
                   touched={touched}
                 />
               </Form>
+
               <Form className="form__field">
                 <FormikField
                   label="Phone"
@@ -113,21 +93,56 @@ export const AddNewUserForm: FC = () => {
                 <ErrorMessage name="position" />
               </Form>
 
-              <Form className="form__field">
-                <Field
-                  name="photo"
-                  type="file"
-                />
-                <ErrorMessage name="photo" />
+              <Form
+                className={classNames(
+                  "form__field form__field--upload",
+                  {"form__field--error":
+                    isTouchedUploadField && errors.photo},
+                )}
+              >
+                <label>
+                  <Field
+                    name="photo"
+                    type="file"
+                    value={undefined}
+                    onChange={(event: UploadEvent) => {
+                      setFieldValue("photo", event.currentTarget.files[0]);
+                      handleUpload(event);
+                    }}
+                    hidden
+                  />
 
-                <button
-                  type="submit"
-                  id="signUp"
-                  disabled={!dirty || !isValid}
-                >
-                  Sign up
-                </button>
+                  <CustomTooltip title={uploadText}>
+                    <div>
+                      <p
+                        className={classNames(
+                          "form__upload-button",
+                          {"form__upload-button--error":
+                            isTouchedUploadField && errors.photo},
+                        )}
+                      >
+                        Upload
+                      </p>
+                      <p className="form__upload-text">
+                        {uploadText}
+                      </p>
+                    </div>
+                  </CustomTooltip>
+                </label>
+                <p className="form__error-message">
+                  {isTouchedUploadField && errors.photo}
+                </p>
               </Form>
+
+              
+              <button
+                type="submit"
+                id="signUp"
+                disabled={!dirty || !isValid}
+                className="button"
+              >
+                Sign up
+              </button>
             </>
 					);
 				}}
