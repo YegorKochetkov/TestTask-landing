8ca74e3c3eb53489826bda50640c285273a65307
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-23 16:19:36 +0300

    Add posting new user

--- src/store/usersApi.ts
+++ src/store/usersApi.ts
@@ -1,6 +1,16 @@
 import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { Users } from "./currentUsersSlice";
 
+export type UpdateUser = {
+  newUser: FormData,
+  token: any,
+};
+
+type GetToken = {
+  success: string,
+  token: any,
+}
+
 export const usersApi = createApi({
 	reducerPath: "usersApi",
 	baseQuery: fetchBaseQuery({baseUrl: "https://frontend-test-assignment-api.abz.agency/api/v1"}),
@@ -15,8 +25,26 @@ export const usersApi = createApi({
 				return (`users?page=${users.page}&count=${users.count}`)
 			},
 			providesTags: [{type: "Users", id: "List"}],
-		})
+		}),
+		getToken: builder.query<GetToken, void>({
+			query: () => {
+				return ("token")
+			},
+      keepUnusedDataFor: 40,
+      providesTags: [{type: "Users", id: "List"}],
+		}),
+    addUser: builder.mutation<any, UpdateUser>({
+      query: (user) => {
+        return {
+          url: "users",
+          method: "POST",
+          body: user.newUser,
+          headers: {"Token": user.token},
+        };
+      },
+      invalidatesTags: [{type: "Users", id: "List"}],
+    }),
 	}),
 });
 
-export const { useGetUsersQuery } = usersApi;
+export const { useGetUsersQuery, useGetTokenQuery, useAddUserMutation } = usersApi;
